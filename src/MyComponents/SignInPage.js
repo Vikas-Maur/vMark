953d7fb68e46f3de5f2a6cf711b2811efcd292cf
Vikas <vikasmaur10@gmail.com>
2022-01-30 15:18:15 +0530

    sign in page created. starting with home page

--- src/MyComponents/SignInPage.js
+++ src/MyComponents/SignInPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import GoogleIcon from "../MyIcons/google-icon.svg";
+import SignInTopImage from "../MyImages/signin-first-img.png";
 import FacebookIcon from "../MyIcons/facebook-icon.svg";
 
 import EllipticalButton from "./MyButtons/EllipticalButton";
@@ -7,14 +8,15 @@ import EllipticalButton from "./MyButtons/EllipticalButton";
 export default function SignInPage({ signInWithGoogle, signInWithFacebook }) {
   return (
     <div className="container h-screen mx-auto flex flex-col items-center">
-      <div className="login-image relative w-full h-3/6">
-        <div className="text-transparent bg-clip-text bg-gradient-to-r from-emerald-600 to-emerald-900 absolute brand left-2/4 bottom-3 -translate-x-1/2 mb-8 text-5xl font-black">
+      <div className="login-image overflow-hidden relative w-full h-3/6 flex justify-center items-center">
+        <img className="opacity-10 aspect-square mt-20 overflow-hidden" src={SignInTopImage} alt="sign in" />
+        <div className="text-transparent bg-clip-text bg-gradient-to-r from-emerald-600 to-emerald-900 absolute brand left-2/4 bottom-3 -translate-x-1/2 mb-2 text-5xl font-black">
           vMark
         </div>
       </div>
       <div className="sign-in w-full h-3/6 flex flex-col items-center">
-        <EllipticalButton onClick={signInWithGoogle} className="font-semibold">
-          <img src={GoogleIcon} alt="google icon" />
+        <EllipticalButton onClick={signInWithGoogle} className="font-semibold mt-8">
+          <img src={GoogleIcon} alt="google icon"/>
           <p className="mx-auto">Continue with google</p>
         </EllipticalButton>
 
